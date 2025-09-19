9d3e1d5ad118fefaa0ed4993a11bf69a77336289
Jithu-SM <jithusmusic@gmail.com>
2025-09-19 10:13:07 +0530

    js basics

--- Day-4/javascript-basics/script.js
+++ Day-4/javascript-basics/script.js
@@ -1,8 +1,29 @@
 console.log("Hello World")
 document.write("Hello World")
 
-var x = 10  //function scoped
-let y = 20  //block scoped
-const z = 30  //block scoped
-
+//var
+var x = "jithu"  //function scoped
 console.log(x)
+var x = 100
+console.log(x)
+
+//let
+let y = 20  //block scoped
+console.log(y)
+// let y = 200  //re-declaration not allowed
+y = 30 
+console.log(y)
+console.log(typeof(y))  //data type
+
+//const
+const PI = 3.14  //block scoped
+console.log(PI)
+// PI = 3.1416  //re-assignment not allowed
+console.log(typeof(PI))  //data type
+
+//object
+const person = {
+    name: "Jithu",
+    age: 21,
+    city: "Kottayam"
+}

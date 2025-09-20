2a67368e0b4567092e81f4c9883316735528425c
Jithu-SM <jithusmusic@gmail.com>
2025-09-20 14:48:20 +0530

    dom

--- Day-5/dom/main.js
+++ Day-5/dom/main.js
@@ -16,3 +16,26 @@ head.classList.add("highlight");
 //remove style from external stylesheet to html element
 para1.classList.remove("underline");
 // para1.classList.toggle("underline");
+
+const leadButton = document.querySelector(".lead")
+// handling events
+leadButton.addEventListener("click",(e)=>{
+    alert("Button is clicked");
+    console.log(e.target)
+})
+
+form = document.querySelector("#signupform")
+form.addEventListener("submit",(e)=>{                   //on submit
+    e.preventDefault()                                  //stop from refreshing
+    const fullName = document.getElementById("fname").value
+    console.log(fullName)
+})
+
+const fname = document.getElementById("fname")
+fname.addEventListener("keydown",(e)=>{                 //on key pressed
+    console.log(e.key)
+})
+
+fname.addEventListener("input",(e)=>{                 
+    console.log(e.target.value)                     //current input value
+})

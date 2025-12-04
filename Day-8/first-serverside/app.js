b2a0907193f5e043af0a761fc5486aed740c6f13
Jithu-SM <jithusmusic@gmail.com>
2025-12-04 13:52:05 +0530

    :)

--- Day-8/first-serverside/app.js
+++ Day-8/first-serverside/app.js
@@ -1,11 +1,23 @@
-const messenger = require('./greetings.js')
+// const messenger = require('./greetings.js')
 
-console.log("Hello from the server")
+// console.log("Hello from the server")
 
-messenger.printer("Jithu")
+// messenger.printer("Jithu")
 
-const os = require('os');
-console.log(`The current working directory is ${os.homedir()}`)
-console.log('The platform is ', os.platform())
-console.log(__dirname)
-console.log(__filename)
+// const os = require('os');
+// console.log(`The current working directory is ${os.homedir()}`)
+// console.log('The platform is ', os.platform())
+// console.log(__dirname)
+// console.log(__filename)
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/products', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
